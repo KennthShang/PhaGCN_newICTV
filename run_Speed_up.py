04406b3c501ff068b6d1de5922423b0a903fe44d
Kenneth Shang <Kenneth.Shang@foxmail.com>
2022-10-14 13:21:31 +0800

    Update run_Speed_up.py

--- run_Speed_up.py
+++ run_Speed_up.py
@@ -58,12 +58,11 @@ else:
 
 try:
     if os.path.exists('database/database.self-diamond.tab'):
-        print(f'Using preformatted DIAMOND database ({diamond_db}) ...')
+        print(f'Using preformatted DIAMOND database ...')
     else:
         make_diamond_cmd = 'diamond makedb --threads 8 --in database/Caudovirales_protein.fasta -d database/database.dmnd'
         print("Creating Diamond database...")
         _ = subprocess.check_call(make_diamond_cmd, shell=True)
-        
         diamond_cmd = 'diamond blastp --threads 8 --sensitive -d database/database.dmnd -q database/Caudovirales_protein.fasta -o database/database.self-diamond.tab'
         print("Running Diamond...")
         _ = subprocess.check_call(diamond_cmd, shell=True)
